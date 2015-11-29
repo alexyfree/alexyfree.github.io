0c08ae7c5469dbceccc587ffc22f0301df94d755
Алексвндр <aleksander@MacBook-Pro-Aleksvndr.local>
2015-11-30 00:39:56 +0300

    add navigation

--- js/app.js
+++ js/app.js
@@ -4,5 +4,23 @@ var alstock = angular.module('alstockApp', []);
 
 alstock.controller('AppCtrl', ['$scope',
     function ($scope) {
-        $scope.alert = "Hello, I'm Angular! How are you going?"
+        var currentActiveTab = 1;
+        $scope.setActiveNavigation = function(number, type){
+            if(type === "click"){
+                $("nav>ul>li:nth-child(" + currentActiveTab + ")>div").removeClass("arrow-right");
+                $("nav>ul>li:nth-child(" + currentActiveTab + ")>a").removeClass("active");
+                currentActiveTab = number;
+            }
+            console.log(1);
+            $( "nav>ul>li:nth-child(" + number + ")>div" ).addClass("arrow-right");
+            $( "nav>ul>li:nth-child(" + number + ")>a" ).addClass("active");
+        };
+
+        $scope.removeActiveNavigation = function(number){
+            console.log(1);
+            if(currentActiveTab !== number) {
+                $("nav>ul>li:nth-child(" + number + ")>div").removeClass("arrow-right");
+                $("nav>ul>li:nth-child(" + number + ")>a").removeClass("active");
+            }
+        };
     }]);
